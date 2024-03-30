06257d49855a304b1b84a87bf2762c19228ca4f5
Nidhi <nidhipoojary1211@gmail.com>
2024-03-31 00:55:06 +0530

    feat : truncate PDF names

--- frontend/src/components/SubjectChat/PdfItem/PdfItem.tsx
+++ frontend/src/components/SubjectChat/PdfItem/PdfItem.tsx
@@ -4,42 +4,46 @@ import { Box } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 
 type Props = {
-	pdf: pdfType;
+    pdf: pdfType;
 };
 
 const PdfItem = ({ pdf }: Props) => {
-	const { deleteDocument } = useContext(StudyContext);
-	return (
-		<Box
-			sx={{
-				display: "flex",
-				justifyContent: "space-between",
-				alignItems: "center",
-				padding: 2,
-				borderBottom: "1px solid rgba(255, 255, 255, 0.12)",
-			}}
-		>
-			<Box
-				sx={{
-					flex: 1,
-					textOverflow: "ellipsis",
-				}}
-			>
-				{pdf.title}
-			</Box>
-			<Box
-				sx={{
-					width: "1rem",
-					cursor: "pointer",
-				}}
-				onClick={() => {
-					deleteDocument(pdf.id);
-				}}
-			>
-				<DeleteIcon />
-			</Box>
-		</Box>
-	);
+    const { deleteDocument } = useContext(StudyContext);
+    const truncateName = (name: string, maxLength: number) => {
+        return name.length > maxLength ? `${name.substring(0, maxLength)}...` : name;
+    };
+
+    return (
+        <Box
+            sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                padding: 2,
+                borderBottom: "1px solid rgba(255, 255, 255, 0.12)",
+            }}
+        >
+            <Box
+                sx={{
+                    flex: 1,
+                    textOverflow: "ellipsis",
+                }}
+            >
+                {truncateName(pdf.title, 20)} 
+            </Box>
+            <Box
+                sx={{
+                    width: "1rem",
+                    cursor: "pointer",
+                }}
+                onClick={() => {
+                    deleteDocument(pdf.id);
+                }}
+            >
+                <DeleteIcon />
+            </Box>
+        </Box>
+    );
 };
 
 export default PdfItem;
